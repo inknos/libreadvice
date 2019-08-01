2320f07bac348d9b5c7c7a1ffd3fd77d07d54fb9
inknos <nicola.sella@outlook.it>
2019-08-02 00:12:54 +0200

    articles

--- libredb/views.py
+++ libredb/views.py
@@ -3,11 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import Application
+
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the db index.")
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the db index.")
-
+    latest_application_list = Application.objects.order_by('-app_name')[:5]
+    template = loader.get_template('libredb/index.html')
+    context = {
+        'latest_application_list': latest_application_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, app_name):
     return HttpResponse("Nome applicazione %s." % app_name)
@@ -18,3 +27,4 @@ def results(request, app_name):
 
 def vote(request, app_name):
     return HttpResponse("You're voting on question %s." % app_name)
+
