82ed195901756b99dc090e9bde1b73b7a902e233
inknos <nicola.sella@outlook.it>
2019-08-03 20:00:04 +0200

    view single app

--- libredb/urls.py
+++ libredb/urls.py
@@ -5,10 +5,12 @@ from . import views
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<str:app_name>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<str:app_name>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    path('<str:app_name>/vote/', views.vote, name='vote'),
+    # ex: /libredb/app/Firefox/
+    path('app/<str:app_name>/', views.detail, name='detail'),
+    # ex: /libredb/Firefox/results/
+    #path('<str:app_name>/results/', views.results, name='results'),
+    # ex: /libredb/Firefox/vote/
+    #path('<str:app_name>/vote/', views.vote, name='vote'),
+    # ex: /libredb/app
+    path('app', views.insert_application, name='insert_application')
 ]
--- libredb/views.py
+++ libredb/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
 
 # Create your views here.
-
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, render
 from django.template import loader
 
 from .models import Application
@@ -11,15 +11,34 @@ from .models import Application
 #    return HttpResponse("Hello, world. You're at the db index.")
 
 def index(request):
-    latest_application_list = Application.objects.order_by('-app_name')[:5]
-    template = loader.get_template('libredb/index.html')
+    latest_application_list = Application.objects.order_by('app_name')[:5]
+    #template = loader.get_template('libredb/index.html')
     context = {
         'latest_application_list': latest_application_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'libredb/index.html', context)
 
 def detail(request, app_name):
-    return HttpResponse("Nome applicazione %s." % app_name)
+    application = get_object_or_404(Application, app_name=app_name)
+    return render(request, 'libredb/detail.html', {
+        'application' : application,
+        'libre' : application.app_libre,
+        'usability' : application.app_usability,
+        'porting' : application.app_porting,
+        'potential' : application.app_potential,
+        'design' : application.app_design,
+        'average' : (
+            int(application.app_libre) +
+            int(application.app_usability) +
+            int(application.app_porting) +
+            int(application.app_potential) +
+            int(application.app_design))/5,
+        'libre_rate' : ['●' if i < int(application.app_libre) else '○' for i in range(5)],
+        'usability_rate' : ['●' if i < int(application.app_usability) else '○' for i in range(5)],
+        'porting_rate' : ['●' if i < int(application.app_porting) else '○' for i in range(5)],
+        'potential_rate' : ['●' if i < int(application.app_potential) else '○' for i in range(5)],
+        'design_rate' : ['●' if i < int(application.app_design) else '○' for i in range(5)],
+})
 
 def results(request, app_name):
     response = "You're looking at the results of question %s."
@@ -28,3 +47,8 @@ def results(request, app_name):
 def vote(request, app_name):
     return HttpResponse("You're voting on question %s." % app_name)
 
+@login_required
+def insert_application(request):
+    #query_results = Application.objects.order_by('app_name')
+
+    return HttpResponse("you are viewing the db")
