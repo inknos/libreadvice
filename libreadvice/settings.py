f651d35bc0daa9186e359879482f69bae67ff133
inknos <nicola.sella@outlook.it>
2019-08-11 11:08:02 +0200

    static files

--- libreadvice/settings.py
+++ libreadvice/settings.py
@@ -145,6 +145,7 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_DIR, 'static'),
 )
 
 # Simplified static file serving.
